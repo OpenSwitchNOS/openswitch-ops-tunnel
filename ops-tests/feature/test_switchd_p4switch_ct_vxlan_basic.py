ac8935a9954067c258c84058bf6fde95fa5de372
Sayali Mane <sayali.p.mane@hpe.com>
2016-10-20 10:27:02 -0700

    Testscases for VxLAN: - Basic testcase - Encap testcase
    Change-Id: I3f6a32189e3fe3e17000398a2e1c9cf335fdbca4
    Signed-off-by: Sayali Mane <sayali.p.mane@hpe.com>

---
+++ ops-tests/feature/test_switchd_p4switch_ct_vxlan_basic.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+#
+# (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+############################################################################
+
+"""
+OpenSwitch Test for VxLAN related configurations.
+"""
+
+from tunnel_ft_utils import *
+from time import sleep
+
+TOPOLOGY = """
+# +-------+                                    +-------+
+# |       |     +--------+      +--------+     |       |
+# |  h1   <---->    s1   <----->   s2    <----->  h2   |
+# |       |     +--------+      +--------+     |       |
+# +-------+                                    +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] s1
+[type=openswitch name="OpenSwitch 1"] s2
+[type=host name="Host 1"] h1
+[type=host name="Host 1"] h2
+
+#ports
+[force_name=oobm] s1:sp1
+
+# Links
+h1:1 -- s1:1
+s1:2 -- s2:2
+s2:1 -- h2:1
+"""
+
+s1_ip = '20.0.0.1'
+s2_ip = '20.0.0.2'
+h1_ip = '10.0.0.10'
+h2_ip = '10.0.0.20'
+mask = '24'
+num_pack = '10'
+vlan = '21'
+vni = '1760'
+tun_num = '10'
+
+
+def nping(host, dest, times, step):
+    step("Starting Ping...")
+    # send contrived traffic
+    # no broadcast, all unicast with well formed mac addrs
+    ping_cmd = ('ping -c ' + times + ' ' + dest)
+    step('Executing ' + ping_cmd)
+
+    output = host(ping_cmd, shell='bash')
+
+    return(output)
+
+
+def test_vxlan(topology, step):
+    """
+    # Test ping between two hosts without VxLAN tunnel creation.
+    # Test ping between two hosts connected through VxLAN tunnel.
+
+    Build a topology of two switches and two hosts and connect
+    the hosts to the switches.
+    Setup a VxLAN between switches and VLANs between ports
+    and switches.
+    Ping from host 1 to host 2.
+    """
+
+    s1 = topology.get('s1')
+    s2 = topology.get('s2')
+    h1 = topology.get('h1')
+    h2 = topology.get('h2')
+
+    assert s1 is not None
+    assert s2 is not None
+    assert h1 is not None
+    assert h2 is not None
+
+    # wait for switchd to come up
+    sleep(10)
+    config_vlan(s1, vlan, step)
+    config_vlan_access_iface(s1, vlan, '1', step)
+    config_l3_iface(s1, '2', s1_ip, mask, step)
+
+    config_vlan(s2, vlan, step)
+    config_vlan_access_iface(s2, vlan, '1', step)
+    config_l3_iface(s2, '2', s2_ip, mask, step)
+
+    # Ping between the two switches
+    step("##### Trying to ping between the two switches #####")
+    output = s1('ip netns exec swns ping -c ' + num_pack + ' ' \
+                + s2_ip, shell='bash')
+
+    step(output)
+    assert(output.find('Destination Host Unreachable') == -1)
+
+    length_pack = len(num_pack)
+    index_t = output.index('packets transmitted') - 1
+    transmitted = (output[index_t - length_pack:index_t])
+    index_r = output.index('received') - 1
+    received = (output[index_r - length_pack:index_r])
+
+    assert(transmitted == received)
+
+    # configure VNI and VLAN to VNI mapping
+    config_vni(s1, vni, step)
+    config_vlan_vni_mapping(s1, vlan, vni, step)
+    config_vni(s2, vni, step)
+    config_vlan_vni_mapping(s2, vlan, vni, step)
+
+    # configure_inter_hs(host, port, ip, step)
+    configure_inter_hs(h1, '1', h1_ip + '/' + mask, step)
+    configure_inter_hs(h2, '1', h2_ip + '/' + mask, step)
+
+    # Negative test
+    # Trying to ping without a VxLAN tunnel creation
+    step("##### Trying to ping between two hosts without tunnel creation #####")
+    neg_ping_output = nping(h1, h2_ip, num_pack, step)
+    step(neg_ping_output)
+
+    assert(neg_ping_output.find('Destination Host Unreachable') != -1)
+
+    length_pack = len(num_pack)
+    index_r = neg_ping_output.index('received') - 1
+    received = (neg_ping_output[index_r - length_pack:index_r])
+    received = [int(s) for s in received.split() if s.isdigit()]
+
+    assert(received[0] == 0)
+
+    # wait for mappings to get resolve
+    sleep(5)
+
+    # Configure tunnel interfaces on both the switches
+    config_tunnel_iface(s1, vni, s1_ip, s2_ip, tun_num, step)
+    config_tunnel_iface(s2, vni, s2_ip, s1_ip, tun_num, step)
+
+    # Wait for tunnel interface to get created
+    sleep(5)
+
+    step("##### Trying to ping between the two hosts #####")
+    ping_output = nping(h1, h2_ip, num_pack, step)
+
+    step(ping_output)
+    assert(ping_output.find('Destination Host Unreachable') == -1)
+
+    length_pack = len(num_pack)
+    index_t = ping_output.index('packets transmitted') - 1
+    transmitted = (ping_output[index_t - length_pack:index_t])
+    index_r = ping_output.index('received') - 1
+    received = (ping_output[index_r - length_pack:index_r])
+
+    assert(transmitted == received)
---
+++ ops-tests/feature/tunnel_ft_utils.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+#
+# (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+############################################################################
+
+"""
+Tunnel utils
+"""
+
+from time import sleep
+
+
+def config_switch(switch, switch_config, step):
+    for config in switch_config:
+        switch(config)
+    sleep(2)
+
+
+def config_vni(switch, vni, step):
+    step("Configuring Switch VNI")
+    switch_config = ["vtysh",
+                     "configure terminal",
+                     "vni {}".format(vni),
+                     "exit",]
+    config_switch(switch, switch_config, step)
+    step("Configuring Switch VNI Completed!")
+
+
+def config_vlan(switch, vlan, step):
+    step("Configuring Switch VLAN")
+    switch_config = ["vtysh",
+                     "configure terminal",
+                     "vlan {}".format(vlan),
+                     "no shut",
+                     "exit",]
+    config_switch(switch, switch_config, step)
+    step("Configuring Switch VLAN Completed!")
+
+
+def config_vlan_vni_mapping(switch, vlan, vni, step):
+    step("Configuring Switch VLAN to VNI Mapping")
+    switch_config = ["vtysh",
+                     "configure terminal",
+                     "vxlan vlan {} vni {}".format(vlan, vni),
+                     "exit",]
+    config_switch(switch, switch_config, step)
+    step("Configuring Switch VLAN to VNI Mapping Completed!")
+
+
+def config_vlan_access_iface(switch, vlan, iface, step):
+    step("Configuring Switch VLAN Access Interface")
+    switch_config = ["vtysh",
+                     "configure terminal",
+                     "interface {}".format(iface),
+                     "no routing",
+                     "vlan access {}".format(vlan),
+                     "no shut",
+                     "exit",]
+    config_switch(switch, switch_config, step)
+    step("Configuring Switch VLAN Access Interface Completed!")
+
+
+def config_l3_iface(switch, iface, ip, mask, step):
+    step("Configuring Switch L3 Interface")
+    switch_config = ["vtysh",
+                     "configure terminal",
+                     "interface {}".format(iface),
+                     "ip address {}/{}".format(ip, mask),
+                     "no shut",
+                     "exit",]
+    config_switch(switch, switch_config, step)
+    step("Configuring Switch L3 Interface Completed!")
+
+
+def config_tunnel_iface(switch, vni_num, s_ip, d_ip, tun_num, step):
+    step("Configuring Switch Tunnel Interface")
+    switch_config = ["vtysh",
+                     "configure terminal",
+                     "interface tunnel {} mode vxlan".format(tun_num),
+                     "source ip {}".format(s_ip),
+                     "destination ip {}". format(d_ip),
+                     "vxlan-vni {}".format(vni_num),
+                     "exit"]
+    config_switch(switch, switch_config, step)
+    step("Configuring Switch Tunnel Interface Completed!")
+
+
+def configure_inter_hs(host, port, ip, step):
+    step("Configuring host")
+    host.libs.ip.interface(port, addr=ip, up=True)
+    host_config = 'ifconfig -a'
+    output = host(host_config, shell='bash')
+    step("Configuring host completed!")
